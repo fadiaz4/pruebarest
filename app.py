00b631ca93eebc0da244888c43c05da695900993
fadiaz4 <72111275+fadiaz4@users.noreply.github.com>
2021-07-12 11:49:57 -0600

    quitarport

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restful import Api
 from resources.medicinas_resource import Medicinas
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
+CORS(app)
 
 api.add_resource(Medicinas, "/medicinas/<int:idmedicinas>")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=23512)
+    app.run(debug=True)
